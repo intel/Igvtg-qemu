6c9b423c8751a0dc37a6ec2dc92bcb45d982e5e8
Jike Song <jike.song@intel.com>
2015-10-15 10:57:35 +0800

    qemu-kvmgt: add GPL license headers
    Signed-off-by: Jike Song <jike.song@intel.com>

--- hw/i386/kvm/kvmgt.h
+++ hw/i386/kvm/kvmgt.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Intel
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ */
 #ifndef _KVM_GT_
 #define _KVM_GT_
 
