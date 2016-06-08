3b5ef91ace45654fbc36db5aedbe5f660756d08d
Zhao Yan <yan.y.zhao@intel.com>
2016-06-08 16:01:39 +0800

    fix the bug that qemu will crash when reboot windows guest frequently
    in xen_ram_addr_from_mapcache() when translating host address ptr to
    guest address, there may exist two matching entries in mapcache->entry of
    the same paddr_index and size. so need to compare more fields to ensure
    the correct entry is found.
    
    Signed-off-by: Zhao Yan <yan.y.zhao@intel.com>
    Signed-off-by: Bing Niu <bing.niu@intel.com>

--- xen-mapcache.c
+++ xen-mapcache.c
@@ -339,7 +339,13 @@ ram_addr_t xen_ram_addr_from_mapcache(void *ptr)
     }
 
     entry = &mapcache->entry[paddr_index % mapcache->nr_buckets];
-    while (entry && (entry->paddr_index != paddr_index || entry->size != size)) {
+    while (entry && (entry->paddr_index != paddr_index || entry->size != size  ||
+            !(((unsigned long)ptr >= (unsigned long)entry->vaddr_base) &&
+              ((unsigned long) ptr < (unsigned long) entry->vaddr_base + (unsigned long)entry->size)) ||
+              !test_bits(((unsigned long) ptr - (unsigned long) entry->vaddr_base) >> XC_PAGE_SHIFT,
+              1,
+             entry->valid_mapping))) {
+
         entry = entry->next;
     }
     if (!entry) {
@@ -385,7 +391,12 @@ static void xen_invalidate_map_cache_entry_unlocked(uint8_t *buffer)
     }
 
     entry = &mapcache->entry[paddr_index % mapcache->nr_buckets];
-    while (entry && (entry->paddr_index != paddr_index || entry->size != size)) {
+    while (entry && (entry->paddr_index != paddr_index || entry->size != size ||
+        !(((unsigned long)buffer >= (unsigned long)entry->vaddr_base)
+        && ((unsigned long) buffer < (unsigned long) entry->vaddr_base + (unsigned long)entry->size)) ||
+        !test_bits(((unsigned long) buffer - (unsigned long) entry->vaddr_base) >> XC_PAGE_SHIFT,
+        1,
+        entry->valid_mapping))) {
         pentry = entry;
         entry = entry->next;
     }
@@ -434,23 +445,26 @@ void xen_invalidate_map_cache(void)
     for (i = 0; i < mapcache->nr_buckets; i++) {
         MapCacheEntry *entry = &mapcache->entry[i];
 
-        if (entry->vaddr_base == NULL) {
-            continue;
-        }
-        if (entry->lock > 0) {
-            continue;
-        }
+        while(entry){
+            if (entry->vaddr_base == NULL) {
+                continue;
+            }
+            if (entry->lock > 0) {
+                continue;
+            }
 
-        if (munmap(entry->vaddr_base, entry->size) != 0) {
-            perror("unmap fails");
-            exit(-1);
-        }
+            if (munmap(entry->vaddr_base, entry->size) != 0) {
+                perror("unmap fails");
+                exit(-1);
+            }
 
-        entry->paddr_index = 0;
-        entry->vaddr_base = NULL;
-        entry->size = 0;
-        g_free(entry->valid_mapping);
-        entry->valid_mapping = NULL;
+            entry->paddr_index = 0;
+            entry->vaddr_base = NULL;
+            entry->size = 0;
+            g_free(entry->valid_mapping);
+            entry->valid_mapping = NULL;
+            entry = entry->next;
+        }
     }
 
     mapcache->last_entry = NULL;
