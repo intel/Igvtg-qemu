ce4e39a5e80033fcf9c98393f52d48a6f43a627f
Tina Zhang <tina.zhang@intel.com>
2019-06-04 13:50:47 +0800

    vfio: Add a funtion to return a specific irq capabilities
    vfio_get_dev_irq_info is introduced to return a specific irq
    capabilities.
    
    Signed-off-by: Tina Zhang <tina.zhang@intel.com>

--- include/hw/vfio/vfio-common.h
+++ include/hw/vfio/vfio-common.h
@@ -191,6 +191,9 @@ int vfio_get_dev_region_info(VFIODevice *vbasedev, uint32_t type,
 bool vfio_has_region_cap(VFIODevice *vbasedev, int region, uint16_t cap_type);
 struct vfio_info_cap_header *
 vfio_get_region_info_cap(struct vfio_region_info *info, uint16_t id);
+
+int vfio_get_dev_irq_info(VFIODevice *vbasedev, uint32_t type,
+                          uint32_t subtype, struct vfio_irq_info **info);
 #endif
 extern const MemoryListener vfio_prereg_listener;
 
